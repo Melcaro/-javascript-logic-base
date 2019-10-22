f09e0121dc1b2b8533854a3ffecda959f9e80693
melanie caro <melanie.caro@stackerine.com>
2019-10-22 12:37:12 +0200

    edit logicBase.js

--- logicBase.js
+++ logicBase.js
@@ -1,3 +1,14 @@
 const age = prompt('Quel âge avez-vous ?');
 const majeur = age >= 18 && console.log('Vous êtes majeur');
-const pasMajeur = age < 18 && console.log('Bonjour simplue user');
+const pasMajeur = age < 18 && console.log('Bonjour simple user');
+
+const userRole = prompt('Quel est votre rôle dans la société ?');
+const isadmin =
+  age >= 18 && userRole === 'admin' && console.log('Bienvenue Admin');
+
+const password = prompt('Quel est votre mot de passe ?');
+const passwordLength =
+  password.length >= 5 &&
+  age >= 18 &&
+  userRole === 'admin' &&
+  console.log('Bienvenue master user');
